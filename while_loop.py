460c18e24c9a2bdf3a5821213b0dfe1cb67cb34d
ashik6582 <bittech8310@gmail.com>
2025-01-23 11:25:23 +0600

    updated

---
+++ while_loop.py
@@ -0,0 +1,32 @@
+# While Loop
+
+# i = 0
+
+# while i<100:
+#     print("Bappy")
+#     i += 1
+
+
+# program to print multiplication table 
+
+"""
+number = int(input("Enter the number of multiplication Table: "))
+
+i = 1
+
+while i<11:
+    print(number, "*", i , "=", number * i)
+    i += 1
+
+"""
+
+
+#while loop with else
+x = 1
+
+while x<4:
+    print(x)
+    x += 1
+
+else:
+    print("Program Executed")
